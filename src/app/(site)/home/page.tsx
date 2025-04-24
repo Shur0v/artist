7c69fdcbcf99bb4d800d48c884e38f093c65614d
shur0v <shurov.0.00.0@gmail.com>
2025-04-24 11:33:47 +0600

    Extra section added

--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -5,7 +5,7 @@ import Poetrysec from '../_components/home/poetrysec';
 import Poetry2sec from '../_components/home/poetry2sec';
 import Eventsec from '../_components/home/eventsec';
 import Artbottom from '../_components/art/page';
-import Slider from '../_components/home/slider';
+// import Slider from '../_components/home/slider';
 
 
 export default function Homee() {
